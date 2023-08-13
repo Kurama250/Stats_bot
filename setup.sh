aef0c1c580652386d92cd0f7f97e0025685cd5b7
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-08-13 18:32:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ apt install npm node.js git -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
 git clone https://github.com/Kurama250/Stats_bot.git
-cd Ghost_bot/
+cd Stats_bot
 npm install discord.js
 npm install pm2 -g
